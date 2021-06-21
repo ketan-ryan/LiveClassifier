bfddd2090a416633622a6f8bc7bbe6d55ddb2861
TheMinecraftOverlordYT <kyurembeats2@gmail.com>
2021-06-21 16:17:26 -0400

    setup framework for video processing and saving

---
+++ camera.py
@@ -0,0 +1,43 @@
+"""
+ Thread safe camera streaming
+ Code is from this gist: https://gist.github.com/allskyee/7749b9318e914ca45eb0a1000a81bf56
+"""
+from threading import Thread, Lock
+import cv2
+
+
+class WebcamVideoStream:
+    def __init__(self, src=0):
+        self.thread = Thread(target=self.update, args=())
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.started = False
+        self.read_lock = Lock()
+
+    def start(self):
+        if self.started:
+            print("already started!!")
+            return None
+        self.started = True
+        self.thread.start()
+        return self
+
+    def update(self):
+        while self.started:
+            (grabbed, frame) = self.stream.read()
+            self.read_lock.acquire()
+            self.grabbed, self.frame = grabbed, frame
+            self.read_lock.release()
+
+    def read(self):
+        self.read_lock.acquire()
+        frame = self.frame.copy()
+        self.read_lock.release()
+        return frame
+
+    def stop(self):
+        self.started = False
+        self.thread.join()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stream.release()
---
+++ video_handler.py
@@ -0,0 +1,57 @@
+import time
+
+from camera import WebcamVideoStream
+import cv2 as cv
+import datetime
+
+cap = WebcamVideoStream().start()
+frames = []
+start_time = datetime.datetime.now().second
+
+
+# Flushes the buffer to a video file
+def save_to_vid(video):
+    path = f"{datetime.datetime.now().strftime('%m-%d-%y %H.%M.%S')}.avi"
+    height, width, _ = video[0].shape
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    # Will have to tweak frame-rate once we figure out how long it takes to process frame with YOLO model
+    out = cv.VideoWriter(path, fourcc,
+                         240, (width, height))
+    for frame in frames:
+        out.write(frame)
+
+    out.release()
+    return path
+
+
+# Handle all frame processing
+def gen_frame():
+    global start_time
+    while cap:
+        frame = cap.read()
+
+        # TODO: This is where to analyze frame with YOLO
+
+        # Draw timestamp in bottom left
+        timestamp = datetime.datetime.now()
+        cv.putText(frame, timestamp.strftime('%b %d %Y, %I:%M:%S %p EST'), (10, frame.shape[0] - 10),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        # Add frame to buffer
+        frames.append(frame)
+
+        # TODO: Change condition to if motion is detected
+        current_time = datetime.datetime.now().second
+        # Need abs because seconds are clamped from 0-60
+        if abs(current_time - start_time) > 3:
+            print(len(frames))
+            # if(len(frames) > xxx) Let the buffer fill before immediately flushing, useful with real condition
+            save_to_vid(frames)
+            frames.clear()
+            start_time = datetime.datetime.now().second
+            continue
+
+        # Convert to byte stream to display on website
+        convert = cv.imencode('.jpg', frame)[1].tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + convert + b'\r\n')
